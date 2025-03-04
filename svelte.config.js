a83290fe5705525f8cb9c6560bcf1ae0d84b3fe4
vladm <vladdmihai4@gmail.com>
2025-03-04 08:54:56 +0200

    new cfg

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
+//import adapter from '@sveltejs/adapter-node';
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
